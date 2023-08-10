fcf2d579a97418dc8c6c5bab11f2cc9a14d1e197
Yevhenii3145 <100586000+Yevhenii3145@users.noreply.github.com>
2023-08-11 00:38:57 +0300

    classes

---
+++ module_10_1/new_lesson/duck_typing.py
@@ -0,0 +1,44 @@
+"""
+Утиная типизация
+Неявная типизация, латентная типизация или утиная типизация (англ. Duck typing) в ООП
+языках - определение факта реализации определённого интерфейса объектом без явного
+указания или наследования этого интерфейса, а просто реализации полного набора методов.
+"""
+
+class Animal:
+    def __init__(self, nickname:str, age: int, owner:str=None) -> None:
+        self.nickname = nickname
+        self.age = age
+        self.owner = owner
+    def sound(self):
+        pass
+    def get_info(self) ->str:
+        return f"It's animal. His name: {self.nickname} and he's {self.age} years old"
+
+class Cat(Animal):
+    def __init__(self, nickname: str, age: int, owner: str):
+        super(Cat,self).__init__(nickname, age)
+        self.owner = owner
+    def sound(self):
+        return f"{self.nickname} says Meow"
+    def get_info(self) -> str:
+        return f"It's cat. His name: {self.nickname} and he's {self.age} years old"
+
+class Dog(Animal):
+    def __init__(self, nickname: str, age: int, owner: str):
+        super(Dog,self).__init__(nickname, age)
+        self.owner = owner
+    def sound(self):
+        return f"{self.nickname} says Woof"
+    def get_info(self) -> str:
+        return f"It's dog. His name: {self.nickname} and he's {self.age} years old"
+
+def pet_say(pet: Animal):
+    print(pet.sound())
+
+animal = Animal("Chupakabra", 100, "Ivan")
+cat = Cat("Boris",4, "Ivan")
+dog = Dog("Rex", 5, "Ivan")
+
+for pet in (animal, cat, dog):
+    pet_say(pet) # None  Boris says Meow  Rex says Woof
---
+++ module_10_1/new_lesson/duck_typing_2.py
@@ -0,0 +1,22 @@
+class Duck:
+    def swim_quack(self):
+        print("I'm a duck and I can swim and quack")
+
+class ToyBird:
+    def swim_quack(self):
+        print("I'm a duck and I can swim and quack")
+
+class Fish:
+    def swim(self):
+        print("I can only swim")
+
+def duck_testing(animal):
+    try:
+        animal.swim_quack()
+    except AttributeError as er:
+        print(f"It is not duck_typing! {er}")
+
+# первые 2 обьекта это утиная типизация, а третий нет
+duck_testing(Duck()) # I'm a duck and I can swim and quack
+duck_testing(ToyBird()) # I'm a duck and I can swim and quack
+duck_testing(Fish()) # It is not duck_typing! 'Fish' object has no attribute 'swim_quack'
